7dbd7b300681221add169f634419249821de50da
sspacelynx <37104290+sspacelynx@users.noreply.github.com>
2021-01-20 14:36:48 +0100

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# toggle-wsl
- A PowerShell 5 script to automate toggling WSL and Hyper-V related virtualization features on Windows 10 Pro.
+# Toggle-WSL
+A PowerShell 5 script to automate toggling WSL and Hyper-V related virtualization features on Windows 10 Pro.
+
+## Usage
+```
+powershell .\toggle-wsl.ps1
+```
+You can make the script easily accessible by creating a shortcut: right click on your desktop -> New -> Shortcut, type `powershell <full-path-to-script>\toggle-wsl.ps1` in the Path field. You can now launch the script by double clicking on the shortcut.
+
+If you have the new Windows Terminal installed you can instead type `wt powershell <full-path-to-script>\toggle-wsl.ps1` in the Path field.
+
+The script expects to find at least one enabled feature. It will not work if all features are already disabled, as there is no saved state to restore! Keep reading to figure out why.
+
+## Why
+A reply to this [stackoverflow thread](https://superuser.com/questions/1208850/why-cant-virtualbox-or-vmware-run-with-hyper-v-enabled-on-windows-10) explains why this script exists:
+
+> VirtualBox and VMware Workstation (and VMware Player) are "level 2 hypervisors". Hyper-V and VMware ESXi are "level 1 hypervisors".  
+The main difference is that a level 2 hypervisor is an application running inside an existing OS, while a level 1 hypervisor is the OS itself.  
+This means that when you enable Hyper-V, your Windows 10 "host" becomes a virtual machine. A special one, but nonetheless a virtual machine.  
+So your question would more aptly be: "Why don't VirtualBox and VMware Workstation work inside a Hyper-V virtual machine?" One can answer because as a VM, the Intel VT-X instruction are no longer accessible from your virtual machine, only the host has access to it.
+
+## How it works
+The script is meant to be an easy tool to toggle between Hyper-V/Windows Subsystem for Linux and other virtualization software (VMware, VirtualBox, etc...)
+
+The script looks for enabled Windows optional features known to cause problems with other virtualization software. It saves their state to a `.json` file, then proceeds to disable them and reboot the system. At this point you can use VMware/VirtualBox/others.  
+When you want to restore features to the precedent state, re-run the script. The script will read the `.json` file, re-enabling only those features that were previously enabled, and then reboot the system.  
+NOTE: if WSL is to be enabled, two reboots are required for it to work properly. The script will detect this and will do it for you.
+
+## List of supported features
+- Hyper-V
+- Windows Hypervisor Platform
+- Virtual Machine Platform
+- Windows Subsystem for Linux
+- Windows Sandbox
+
+Feedback is welcome! If you find any other feature you believe should be disabled but the script does not, please open an issue and I'll gladly add it to the list.
---
+++ toggle-wsl.ps1
@@ -0,0 +1,167 @@
+﻿<#  Toggle-WSL
+    
+    A PowerShell 5 script to automate toggling WSL and Hyper-V related virtualization features on Windows 10 Pro.
+
+    Made by https://github.com/sspacelynx
+#>
+
+[String[]] $featureList = "Microsoft-Windows-Subsystem-Linux", "VirtualMachinePlatform", "Microsoft-Hyper-V-All", "HypervisorPlatform", "Containers-DisposableClientVM"
+[String] $savedStatePath = "$env:LOCALAPPDATA\toggle-wsl.json"
+[String] $startupPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\toggle-wsl.cmd"
+[Int] $global:defaultPadding = 10
+[Console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+$host.UI.RawUI.WindowTitle = "Toggle-WSL"
+
+
+<# Class / Functions Definitions #>
+
+function Elevate
+{
+    if ($env:WT_SESSION)
+    {
+        Start-Process -Verb RunAs wt "powershell C:\DEV\GitHub\toggle-wsl\toggle-wsl.ps1"
+    }
+    else
+    {
+        Start-Process -Verb RunAs powershell C:\DEV\GitHub\toggle-wsl\toggle-wsl.ps1
+    }
+}
+
+function Test-Administrator
+{  
+    [Security.Principal.WindowsPrincipal] $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    return $user.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator);
+}
+
+function GetfState([String] $feature)
+{
+    return (Get-WindowsOptionalFeature -Online -FeatureName $feature).State -eq "Enabled"
+}
+
+function GetDisplayName([String] $feature)
+{
+    return (Get-WindowsOptionalFeature -Online -FeatureName $feature).DisplayName
+}
+
+function SaveState($obj)
+{
+    $obj | ConvertTo-Json | Out-File $savedStatePath
+}
+
+function LoadState()
+{
+    return Get-Content -Path $savedStatePath -Raw | ConvertFrom-Json
+}
+
+function DeleteSavedState()
+{
+    Remove-Item -Path $savedStatePath
+}
+
+
+<# Main #>
+
+Write-Host "     ╭──────────────────╮
+---=/│    TOGGLE-WSL    │\=---
+---=\│       v1.0       │/=---
+     ╰──────────────────╯`n"
+
+if(-not (Test-Administrator))
+{
+    Write-Warning "This script must be executed as Administrator!"
+    Read-Host -Prompt "Press enter to restart the script with administrator privileges..." | Out-Null
+    Elevate
+    exit 0
+}
+else
+{
+    [IO.FileInfo] $file = $savedStatePath
+
+    Write-Host "Settings
+‾‾‾‾‾‾‾‾
+Saved State Path: $savedStatePath
+Startup Batch Path: $startupPath"
+
+    if ($file.Exists -and $file.Length -gt 0) # Save state found. All features are likely disabled, restore from saved state
+    {
+        Write-Host "`nSaved state found! Loading features state."
+        $savestate = LoadState
+        Write-Host "`nFollowing features will be enabled:"
+        foreach ($s in $savestate)
+        {
+            # Check if double restart is needed
+            if ($s.state)
+            {
+                Write-Host "-> $(GetDisplayName($s.name))"
+                if ($s.name -eq "Microsoft-Windows-Subsystem-Linux")
+                {
+                    Set-Content -Path $startupPath -Value "shutdown /t 5 /r /c `"Your PC will reboot in 5 seconds to apply changes. This is the second reboot.`"`n(goto) 2>nul & del `"%~f0`""
+                }
+            }
+        }
+
+        Read-Host -Prompt "`nAre you sure you want to enable features and reboot? Press enter to continue..." | Out-Null
+
+        foreach ($feature in $savestate)
+        {
+            if ($feature.state)
+            {
+                Enable-WindowsOptionalFeature -Online -FeatureName $feature.name -NoRestart 3>&1 | Out-Null
+            }
+        }
+
+        Write-Host "`nDeleting saved state..."
+        DeleteSavedState
+        shutdown /t 5 /r /c "Your PC will reboot TWICE in 5 seconds to apply changes. This is the first reboot, please wait for the second reboot to be completed before using your PC."
+    }
+    else # Save state not found. Find enabled features, disable them and save state
+    {
+        $features = @()
+        $exit = $true
+
+        foreach ($feature in $featureList)
+        {
+            $f = New-Object PSObject -Property @{
+                name = $feature
+                state = GetfState($feature)
+            }
+
+            # If all features are disabled, set exit flag
+            if ($f.state)
+            {
+                if ($exit)
+                {
+                    $exit = $false
+                    Write-Host "`nEnabled features found:"
+                }
+                Write-Host "-> $(GetDisplayName($f.name))"
+            }
+
+            $features += $f
+        }
+        
+        if ($exit)
+        {
+            Write-Host "`nERROR! All features are disabled and no saved state was found, the script cannot continue. Aborting."
+            Read-Host -Prompt "Press enter to quit..." | Out-Null
+            exit 0
+        }
+
+        Read-Host -Prompt "`nAre you sure you want to disable features and reboot? Press enter to continue..." | Out-Null
+
+        foreach ($feature in $features)
+        {
+            if ($feature.state)
+            {
+                Disable-WindowsOptionalFeature -Online -FeatureName $feature.name -NoRestart 3>&1 | Out-Null
+            }
+        }
+
+        Write-Host "`nSaving state..."
+        SaveState($features)
+        shutdown /t 5 /r /c "Your PC will reboot in 5 seconds to apply changes."
+    }
+
+    Write-Host "`nDone! Rebooting."
+    Start-Sleep -s 3
+}
