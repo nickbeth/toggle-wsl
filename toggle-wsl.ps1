ee6639ab565615d619f0e85ab08d36397178bc72
sspacelynx <37104290+sspacelynx@users.noreply.github.com>
2021-03-22 09:54:06 +0100

    Code formatting: switch to K&R Stroustrup brackets style
    Code and comments cleanup.

--- toggle-wsl.ps1
+++ toggle-wsl.ps1
@@ -1,5 +1,5 @@
-﻿<#  Toggle-WSL
-    
+<#  Toggle-WSL
+
     A PowerShell 5 script to automate toggling WSL and Hyper-V related virtualization features on Windows 10 Pro.
 
     Made by https://github.com/sspacelynx
@@ -8,53 +8,43 @@
 [String[]] $featureList = "Microsoft-Windows-Subsystem-Linux", "VirtualMachinePlatform", "Microsoft-Hyper-V-All", "HypervisorPlatform", "Containers-DisposableClientVM"
 [String] $savedStatePath = "$env:LOCALAPPDATA\toggle-wsl.json"
 [String] $startupPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\toggle-wsl.cmd"
-[Int] $global:defaultPadding = 10
 [Console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 $host.UI.RawUI.WindowTitle = "Toggle-WSL"
 
 
 <# Class / Functions Definitions #>
 
-function Elevate
-{
-    if ($env:WT_SESSION)
-    {
+function Elevate {
+    if ($env:WT_SESSION) {
         Start-Process -Verb RunAs wt "powershell $PSScriptRoot\toggle-wsl.ps1"
     }
-    else
-    {
+    else {
         Start-Process -Verb RunAs powershell $PSScriptRoot\toggle-wsl.ps1
     }
 }
 
-function Test-Administrator
-{  
+function Test-Administrator {
     [Security.Principal.WindowsPrincipal] $user = [Security.Principal.WindowsIdentity]::GetCurrent();
     return $user.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator);
 }
 
-function GetfState([String] $feature)
-{
+function GetfState([String] $feature) {
     return (Get-WindowsOptionalFeature -Online -FeatureName $feature).State -eq "Enabled"
 }
 
-function GetDisplayName([String] $feature)
-{
+function GetDisplayName([String] $feature) {
     return (Get-WindowsOptionalFeature -Online -FeatureName $feature).DisplayName
 }
 
-function SaveState($obj)
-{
+function SaveState($obj) {
     $obj | ConvertTo-Json | Out-File $savedStatePath
 }
 
-function LoadState()
-{
+function LoadState() {
     return Get-Content -Path $savedStatePath -Raw | ConvertFrom-Json
 }
 
-function DeleteSavedState()
-{
+function DeleteSavedState() {
     Remove-Item -Path $savedStatePath
 }
 
@@ -66,111 +56,98 @@ Write-Host "     ╭──────────────────╮
 ---=\│       v1.0       │/=---
      ╰──────────────────╯`n"
 
-if(-not (Test-Administrator))
-{
+if(-not (Test-Administrator)) {
     Write-Warning "This script must be executed as Administrator!"
     Read-Host -Prompt "Press enter to restart the script with administrator privileges..." | Out-Null
     Elevate
     exit 0
 }
-else
-{
-    [IO.FileInfo] $file = $savedStatePath
 
-    Write-Host "Settings
+[IO.FileInfo] $file = $savedStatePath
+
+Write-Host "Settings
 ‾‾‾‾‾‾‾‾
 Saved state: $savedStatePath
 Double reboot script: $startupPath"
 
-    if ($file.Exists -and $file.Length -gt 0) # Save state found. All features are likely disabled, restore from saved state
-    {
-        [Bool] $doubleRestart = $false
-        Write-Host "`nSaved state found! Loading features state."
-        $savestate = LoadState
-        Write-Host "`nFollowing features will be enabled:"
-        foreach ($s in $savestate)
-        {
-            # Check if double restart is needed
-            if ($s.state)
-            {
-                [String] $m = "-> $(GetDisplayName($s.name))"
-                if ($s.name -eq "Microsoft-Windows-Subsystem-Linux")
-                {
-                    $doubleRestart = $true
-                    $m += " (2 rebooots needed)"
-                }
-                Write-Host $m
+if ($file.Exists -and $file.Length -gt 0) {
+    # case: saved state found -> all features are likely disabled, restore from saved state
+    [Bool] $doubleRestart = $false
+    Write-Host "`nSaved state found! Loading features state."
+    $savestate = LoadState
+
+    Write-Host "`nFollowing features will be enabled:"
+    foreach ($s in $savestate) {
+        if ($s.state) {
+            [String] $m = "-> $(GetDisplayName($s.name))"
+
+            # check if double restart is needed
+            if ($s.name -eq "Microsoft-Windows-Subsystem-Linux") {
+                $doubleRestart = $true
+                $m += " (2 reboots needed)"
             }
+            Write-Host $m
         }
-
-        Read-Host -Prompt "`nAre you sure you want to enable features and reboot? Press enter to continue..." | Out-Null
-
-        foreach ($feature in $savestate)
-        {
-            if ($feature.state)
-            {
-                Enable-WindowsOptionalFeature -Online -FeatureName $feature.name -NoRestart 3>&1 | Out-Null
-            }
-        }
-        
-        if ($doubleRestart)
-        {
-            Set-Content -Path $startupPath -Value "shutdown /t 5 /r /c `"Your PC will reboot in 5 seconds to apply changes. This is the second reboot.`"`n(goto) 2>nul & del `"%~f0`""
-            Write-Host "`nDouble reboot script created."
-        }
-
-        Write-Host "`nDeleting saved state..."
-        DeleteSavedState
-        shutdown /t 5 /r /c "Your PC will reboot TWICE in 5 seconds to apply changes. This is the first reboot, please wait for the second reboot to be completed before using your PC."
-    }
-    else # Save state not found. Find enabled features, disable them and save state
-    {
-        $features = @()
-        $exit = $true
-
-        foreach ($feature in $featureList)
-        {
-            $f = New-Object PSObject -Property @{
-                name = $feature
-                state = GetfState($feature)
-            }
-
-            # If all features are disabled, set exit flag
-            if ($f.state)
-            {
-                if ($exit)
-                {
-                    $exit = $false
-                    Write-Host "`nEnabled features found:"
-                }
-                Write-Host "-> $(GetDisplayName($f.name))"
-            }
-
-            $features += $f
-        }
-        
-        if ($exit)
-        {
-            Write-Host "`nERROR! All features are disabled and no saved state was found, the script cannot continue. Aborting."
-            Read-Host -Prompt "Press enter to quit..." | Out-Null
-            exit 0
-        }
-
-        Read-Host -Prompt "`nAre you sure you want to disable features and reboot? Press enter to continue..." | Out-Null
-
-        foreach ($feature in $features)
-        {
-            if ($feature.state)
-            {
-                Disable-WindowsOptionalFeature -Online -FeatureName $feature.name -NoRestart 3>&1 | Out-Null
-            }
-        }
-
-        Write-Host "`nSaving state..."
-        SaveState($features)
-        shutdown /t 5 /r /c "Your PC will reboot in 5 seconds to apply changes."
     }
 
-    Write-Host "`nDone! Rebooting."
-    Start-Sleep -s 3
+    Read-Host -Prompt "`nAre you sure you want to enable features and reboot? Press enter to continue..." | Out-Null
+
+    foreach ($feature in $savestate) {
+        if ($feature.state) {
+            Enable-WindowsOptionalFeature -Online -FeatureName $feature.name -NoRestart 3>&1 | Out-Null
+        }
+    }
+
+    if ($doubleRestart) {
+        Set-Content -Path $startupPath -Value "shutdown /t 5 /r /c `"Your PC will reboot in 5 seconds to apply changes. This is the second reboot.`"`n(goto) 2>nul & del `"%~f0`""
+        Write-Host "`nDouble reboot script created."
+    }
+
+    Write-Host "`nDeleting saved state..."
+    DeleteSavedState
+    shutdown /t 5 /r /c "Your PC will reboot TWICE in 5 seconds to apply changes. This is the first reboot, please wait for the second reboot to be completed before using your PC."
 }
+else {
+    # case: saved state not found -> find enabled features, disable them and save state
+    $features = @()
+    $exit = $true
+
+    foreach ($feature in $featureList) {
+        $f = New-Object PSObject -Property @{
+            name = $feature
+            state = GetfState($feature)
+        }
+
+        # If all features are disabled, set exit flag
+        if ($f.state) {
+            if ($exit) {
+                $exit = $false
+                Write-Host "`nEnabled features found:"
+            }
+            Write-Host "-> $(GetDisplayName($f.name))"
+        }
+
+        $features += $f
+    }
+
+    if ($exit) {
+        Write-Host "`nERROR! All features are disabled and no saved state was found, the script cannot continue. Aborting."
+        Read-Host -Prompt "Press enter to quit..." | Out-Null
+        exit 0
+    }
+
+    Read-Host -Prompt "`nAre you sure you want to disable features and reboot? Press enter to continue..." | Out-Null
+
+    foreach ($feature in $features) {
+        if ($feature.state) {
+            Disable-WindowsOptionalFeature -Online -FeatureName $feature.name -NoRestart 3>&1 | Out-Null
+        }
+    }
+
+    Write-Host "`nSaving state..."
+    SaveState($features)
+    shutdown /t 5 /r /c "Your PC will reboot in 5 seconds to apply changes."
+}
+
+Write-Host "`nDone! Rebooting."
+Start-Sleep -s 3
